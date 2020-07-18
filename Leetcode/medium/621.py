e7e66fcc24cc60cf7d9ea1b8bec60df793611548
luoyanhan <707025023@qq.com>
2020-07-18 10:36:13 +0800

    feat(Leetcode Medium): 621 bucket method

--- Leetcode/medium/621.py
+++ Leetcode/medium/621.py
@@ -1,3 +1,14 @@
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        pass
+    def leastInterval(self, tasks, n: int):
+        import collections
+        ct = collections.Counter(tasks)
+        nbucket = ct.most_common(1)[0][1]
+        res = (nbucket-1)*(n+1)
+        res += list(ct.values()).count(nbucket)
+        return max(res, len(tasks))
+
+
+
+
+
+
