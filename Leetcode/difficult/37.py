45c8918e53e4da2ef1ac23d4cb4b949a48e198a7
罗延瀚 <luoyanhan@alphaleader.com>
2020-07-24 10:16:47 +0800

    feat(Leetcode medium): 37 rewrite

--- Leetcode/difficult/37.py
+++ Leetcode/difficult/37.py
@@ -52,6 +52,59 @@ class Solution:
         sloved = False
         backtrace(0, 0)
 
+class Solution:   #mine rewrite
+    def solveSudoku(self, board):
+        n = 3
+        rows = [[False]*(n*n+1) for i in range(n*n)]
+        cols = [[False]*(n*n+1) for i in range(n*n)]
+        boxes = [[False]*(n*n+1) for i in range(n*n)]
+        flag = False
+
+        def get_box_index(x, y):
+            return x//n*n + y//n
+
+        def place(num, x, y):
+            rows[x][num] = True
+            cols[y][num] = True
+            boxes[get_box_index(x, y)][num] = True
+            board[x][y] = str(num)
+
+        def remove(num, x, y):
+            rows[x][num] = False
+            cols[y][num] = False
+            boxes[get_box_index(x, y)][num] = False
+            board[x][y] = '.'
+
+        def could_place(num, x, y):
+            return not rows[x][num] and not cols[y][num] and not boxes[get_box_index(x, y)][num]
+
+        def backtrack(x, y):
+            if board[x][y] == '.':
+                for num in range(1, 10):
+                    if could_place(num, x, y):
+                        place(num, x, y)
+                        judge_next(x, y)
+                        if not flag:
+                            remove(num, x, y)
+            else:
+                judge_next(x, y)
+
+        def judge_next(x, y):
+            if x == n*n-1 and y == n*n-1:
+                nonlocal flag
+                flag = True
+            else:
+                if y == n*n-1:
+                    backtrack(x+1, 0)
+                else:
+                    backtrack(x, y+1)
+
+        for x in range(n*n):
+            for y in range(n*n):
+                if board[x][y] != '.':
+                    place(int(board[x][y]), x, y)
+        backtrack(0, 0)
+
 
 Solution().solveSudoku([["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]])
 
