adde7b664f60cb35bf68c1843538cfd9adfc4dda
罗延瀚 <luoyanhan@alphaleader.com>
2020-08-03 10:54:14 +0800

    feat(Leetcode medium): 137 method easy to understand

--- Leetcode/medium/137.py
+++ Leetcode/medium/137.py
@@ -6,3 +6,16 @@ class Solution:
             one = (num ^ one) & ~two
             two = (num ^ two) & ~one
         return one
+
+
+class Solution:
+    def singleNumber(self, nums):
+        one = 0
+        two = 0
+        for num in nums:
+            two |= num & one
+            one = one ^ num
+            three = one & two
+            one = one & ~three
+            two = two & ~three
+        return one
