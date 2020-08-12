a48327330487e756d64406148d72dcd698670c54
罗延瀚 <luoyanhan@alphaleader.com>
2020-08-12 16:12:09 +0800

    feat(Leetcode medium): 142(fast and slow ptr method)

--- Leetcode/medium/142.py
+++ Leetcode/medium/142.py
@@ -14,3 +14,26 @@ class Solution:
             visited.add(node)
             node = node.next
         return None
+
+
+class Solution:
+    def detectCycle(self, head):
+        node1 = head
+        node2 = head
+        while True:
+            if not node2 or not node2.next:
+                return None
+            node1 = node1.next
+            node2 = node2.next.next
+            if node1 == node2:
+                break
+        ptr1 = node1
+        ptr2 = head
+        while True:
+            if ptr1 == ptr2:
+                return ptr1
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+
+
