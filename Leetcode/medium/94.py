0d7a9ce6eb28b61b8a425beabcaa59c1ddf6f174
罗延瀚 <luoyanhan@alphaleader.com>
2020-08-13 18:04:16 +0800

    feat(Leetcode medium): 94

---
+++ Leetcode/medium/94.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        res = list()
+        stack = list()
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
+
+
+class Solution:   #莫里斯算法
+    def inorderTraversal(self, root):
+        res = list()
+        cur = root
+        while cur:
+            if not cur.left:
+                res.append(cur.val)
+                cur = cur.right
+            else:
+                pre = cur.left
+                while pre.right and pre.right != cur:
+                    pre = pre.right
+                if not pre.right:
+                    pre.right = cur
+                    cur = cur.left
+                else:
+                    res.append(cur.val)
+                    cur = cur.right
+                    pre.right = None
+        return res
