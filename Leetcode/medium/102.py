04c6d22fbc2cce525201e2208f55de5422442d6b
罗延瀚 <luoyanhan@alphaleader.com>
2020-08-13 15:49:53 +0800

    feat(Leetcode medium): 102, 144

---
+++ Leetcode/medium/102.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return list()
+        q = [root]
+        res = list()
+        while q:
+            next_level = list()
+            res.append([node.val for node in q])
+            for i in range(len(q)):
+                node = q.pop(0)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
+
+
+
