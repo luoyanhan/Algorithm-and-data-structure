506f55b2c5e43758ec8deb239c9fbe7345296a4c
luoyanhan <707025023@qq.com>
2020-08-17 22:20:48 +0800

    feat(Leetcode Medium): 889

--- Leetcode/medium/889.py
+++ Leetcode/medium/889.py
@@ -17,6 +17,24 @@ class Solution:
         root.right = self.constructFromPrePost(pre[L+1:], post[L: -1])
         return root
 
+class Solution:
+    def constructFromPrePost(self, pre, post):
+        def make(l0, l1, N):
+            if not N:
+                return None
+            root = TreeNode(pre[l0])
+            if N == 1:
+                return root
+            L = 0
+            while L < N:
+                if pre[l0+1] == post[l1-1+L]:
+                    break
+                L += 1
+            root.left = make(l0+1, l1, L)
+            root.right = make(l0+1+L, l1+L, N-L-1)   #N-L-1的1容易忽略
+            return root
+        return make(0, 0, len(pre))
+
 
 
 
