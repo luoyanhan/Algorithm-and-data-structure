6df8bd3cbd3aa3b392c75737cd22d488436ddadd
罗延瀚 <luoyanhan@alphaleader.com>
2020-08-27 15:16:30 +0800

    feat(Leetcode medium):5

--- Leetcode/medium/5.py
+++ Leetcode/medium/5.py
@@ -18,3 +18,22 @@ class Solution:
                     res = s[j: end+1]
         return res
 
+
+class Solution:
+    def longestPalindrome(self, s):
+        start, end = 0, 0
+        length = len(s)
+        def inner(left, right):
+            while left >= 0 and right < length and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+        for i in range(length):
+            left1, right1 = inner(i, i)
+            left2, right2 = inner(i, i+1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end+1]
+
