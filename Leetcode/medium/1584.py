9cb41fa099c03afa9d1d6405173eac09fb5163fd
罗延瀚 <luoyanhan@alphaleader.com>
2020-12-17 16:29:48 +0800

    feat(Leetcode medium):1584 add kruskal method

--- Leetcode/medium/1584.py
+++ Leetcode/medium/1584.py
@@ -43,9 +43,55 @@ class Solution:
         return res
 
 
+#kruskal
+class Solution:
+    def minCostConnectPoints(self, points):
+        MST = list()   #用来存储最小生成树路径， 答题不需要
+        res = 0
+        roads = list()
+        num = 0
+        length = len(points)
+        father_map = {i: i for i in range(length)}   #用点在points数组的下标代替(x, y)形式，可以减少执行时间和空间
+        def find_father(point_idx):
+            if father_map[point_idx] != point_idx:
+                father_map[point_idx] = find_father(father_map[point_idx])
+            return father_map[point_idx]
+        # def find_father(point_idx):
+        #     while point_idx != father_map[point_idx]:
+        #         point = father_map[point_idx]
+        #     return father_map[point_idx]
+        # def union(point1_idx, point2_idx):
+        #     father1 = find_father(point1_idx)
+        #     father2 = find_father(point2_idx)
+        #     if father1 != father2:
+        #         if father1 < father2:
+        #             father_map[father1] = father2
+        #         else:
+        #             father_map[father2] = father1
+        def union(point1_idx, point2_idx):
+            father1 = find_father(point1_idx)
+            father2 = find_father(point2_idx)
+            if father1 != father2:
+                father_map[father1] = father2
+        for i in range(length):
+            for j in range(i+1, length):
+                point1 = points[i]
+                point2 = points[j]
+                roads.append(((i, j), abs(point1[0]-point2[0]) +
+                              abs(point1[1]-point2[1])))
+        roads.sort(key=lambda x: x[1])
+        for each in roads:
+            start_idx, end_idx = each[0]
+            if find_father(start_idx) != find_father(end_idx):
+                res += each[1]
+                MST.append(each[0])
+                num += 1
+                if num == length - 1:
+                    return res
+                union(start_idx, end_idx)
+        return res
 
 
-
-print(Solution().minCostConnectPoints())
+print(Solution().minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]]))
 
 
