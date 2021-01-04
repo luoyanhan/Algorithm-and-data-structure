2f90f5ba6107c08ce0c4ccf184c89dd17cb1e430
罗延瀚 <luoyanhan@alphaleader.com>
2021-01-04 15:24:21 +0800

    feat(Leetcode medium):437 前缀和

--- Leetcode/medium/437.py
+++ Leetcode/medium/437.py
@@ -72,3 +72,24 @@ class Solution:
             res = 1 if new_total == sum else 0
             return res + count_path(node.left, new_total) + count_path(node.right, new_total)
         return count_path(root, 0) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+
+
+class Solution:
+    def pathSum(self, root, sum):
+        pre_sum_count = {0: 1}
+
+        def dfs(node, total_sum):
+            res = 0
+            if not node:
+                return 0
+            current_sum = total_sum + node.val
+            res += pre_sum_count.get(current_sum-sum, 0)
+            pre_sum_count[current_sum] = pre_sum_count.get(current_sum, 0) + 1
+            res += dfs(node.left, current_sum)
+            res += dfs(node.right, current_sum)
+            pre_sum_count[current_sum] -= 1
+            return res
+        return dfs(root, 0)
+
+
+
