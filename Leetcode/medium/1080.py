d58b4166b2c14a39e2e83887286eee1cdfc12524
罗延瀚 <luoyanhan@alphaleader.com>
2021-01-07 09:53:42 +0800

    feat(Leetcode medium):1080

--- Leetcode/medium/1080.py
+++ Leetcode/medium/1080.py
@@ -7,4 +7,21 @@
 
 class Solution:
     def sufficientSubset(self, root, limit):
-        pass
+        def post_order(node, cur_sum):
+            if not node.left and not node.right:
+                return cur_sum + node.val >= limit
+            res_left = False
+            res_right = False
+            if node.left:
+                res_left = post_order(node.left, cur_sum+node.val)
+            if node.right:
+                res_right = post_order(node.right, cur_sum+node.val)
+            if not res_left:
+                node.left = None
+            if not res_right:
+                node.right = None
+            return res_left or res_right
+        res_root = post_order(root, 0)
+        if not res_root:
+            return None
+        return root
