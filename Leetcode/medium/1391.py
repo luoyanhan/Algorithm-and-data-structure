3b4b2981ee4c7f3a37d0f2d282c1f8df67004035
罗延瀚 <luoyanhan@alphaleader.com>
2021-01-07 14:49:00 +0800

    feat(Leetcode medium):1391

--- Leetcode/medium/1391.py
+++ Leetcode/medium/1391.py
@@ -45,4 +45,54 @@ class Solution:
         return find_father(left) == find_father(right)
 
 
+
+class Solution:
+    def hasValidPath(self, grid):
+        rows, cols = len(grid), len(grid[0])
+        father_map = [_ for _ in range(rows*cols)]
+        def find_father(node):
+            if father_map[node] != node:
+                father_map[node] = find_father(father_map[node])
+            return father_map[node]
+        def union(node1, node2):
+            father1 = find_father(node1)
+            father2 = find_father(node2)
+            father_map[father1] = father2
+
+        for i in range(rows):
+            for j in range(cols):
+                node_idx = i*cols + j
+                if grid[i][j] == 1:
+                    if j > 0 and grid[i][j-1] in [1, 4, 6]:
+                        union(node_idx, node_idx-1)
+                    if j+1 < cols and grid[i][j + 1] in [1, 3, 5]:
+                        union(node_idx, node_idx + 1)
+                elif grid[i][j] == 2:
+                    if i > 0 and grid[i-1][j] in [2, 3, 4]:
+                        union(node_idx, node_idx - cols)
+                    if i+1 < rows and grid[i + 1][j] in [2, 5, 6]:
+                        union(node_idx, node_idx + cols)
+                elif grid[i][j] == 3:
+                    if j > 0 and grid[i][j - 1] in [1, 4, 6]:
+                        union(node_idx, node_idx - 1)
+                    if i + 1 < rows and grid[i + 1][j] in [2, 5, 6]:
+                        union(node_idx, node_idx + cols)
+                elif grid[i][j] == 4:
+                    if j + 1 < cols and grid[i][j + 1] in [1, 3, 5]:
+                        union(node_idx, node_idx + 1)
+                    if i + 1 < rows and grid[i + 1][j] in [2, 5, 6]:
+                        union(node_idx, node_idx + cols)
+                elif grid[i][j] == 5:
+                    if j > 0 and grid[i][j - 1] in [1, 4, 6]:
+                        union(node_idx, node_idx - 1)
+                    if i > 0 and grid[i - 1][j] in [2, 3, 4]:
+                        union(node_idx, node_idx - cols)
+                elif grid[i][j] == 6:
+                    if j + 1 < cols and grid[i][j + 1] in [1, 3, 5]:
+                        union(node_idx, node_idx + 1)
+                    if i > 0 and grid[i - 1][j] in [2, 3, 4]:
+                        union(node_idx, node_idx - cols)
+        return find_father(0) == find_father(rows*cols-1)
+
+
 print(Solution().hasValidPath([[6,1,3],[4,1,5]]))
