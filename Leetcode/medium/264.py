323e1dc09de66343674ae605b0f1aee9919a7265
luoyanhan <707025023@qq.com>
2021-02-06 17:59:22 +0800

    feat(Leetcode mediun):313, 264 add 动态规划(重要)

--- Leetcode/medium/264.py
+++ Leetcode/medium/264.py
@@ -16,3 +16,24 @@ class Solution:
                     visited.add(new_ugly)
                     heappush(heap, new_ugly)
 
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        nums = [1]
+        cnt = 1
+        i2 = 0
+        i3 = 0
+        i5 = 0
+        while cnt < n:
+            nums.append(min(2*nums[i2], 3*nums[i3], 5*nums[i5]))
+            if nums[-1] == 2*nums[i2]:
+                i2 += 1
+            if nums[-1] == 3*nums[i3]:
+                i3 += 1
+            if nums[-1] == 5*nums[i5]:
+                i5 += 1
+            cnt += 1
+        return nums[-1]
+
+print(Solution().nthUglyNumber(10))
+
