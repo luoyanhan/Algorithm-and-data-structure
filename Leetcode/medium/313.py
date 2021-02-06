6ce2a0b58e14789252ba3d4d3ec2068a8b675346
luoyanhan <707025023@qq.com>
2021-02-06 17:59:07 +0800

    feat(Leetcode mediun):313, 264 add 动态规划(重要)

--- Leetcode/medium/313.py
+++ Leetcode/medium/313.py
@@ -15,3 +15,18 @@ class Solution:
                 if new_ugly not in visited:
                     visited.add(new_ugly)
                     heappush(heap, new_ugly)
+
+
+class Solution:
+    def nthSuperUglyNumber(self, n, primes):
+        nums = [1]
+        cnt = 1
+        length = len(primes)
+        points = [0 for i in range(length)]
+        while cnt < n:
+            nums.append(min([primes[i]*nums[points[i]] for i in range(length)]))
+            for j in range(length):
+                if nums[-1] == primes[j]*nums[points[j]]:
+                    points[j] += 1
+            cnt += 1
+        return nums[-1]
