39e1e9ff177b38a77d9f22712c5a6f499d5ce7ef
luoyanhan <707025023@qq.com>
2021-04-20 10:09:15 +0800

    feat(Leetcode medium):184 fix code

--- Leetcode/medium/184.py
+++ Leetcode/medium/184.py
@@ -1,5 +1,4 @@
-select Department.Name as `Department`, Employee.Name as `Employee`, Employee.Salary from Employee join Department on Employee.DepartmentId = Department.Id and (Employee.Salary, Employee.DepartmentId) in
-(select max(Salary), DepartmentId from Employee group by DepartmentId);
+'''select Department.Name as `Department`, Employee.Name as `Employee`, Employee.Salary from Employee join Department on Employee.DepartmentId = Department.Id and (Employee.Salary, Employee.DepartmentId) in (select max(Salary), DepartmentId from Employee group by DepartmentId);'''
 
 
-select Department.Name as `Department`, aa.Name as `Employee`, aa.Salary from (select a.Name, a.Salary, a.DepartmentId from Employee a join (select DepartmentId, max(Salary) as max_s from Employee group by DepartmentId) b on a.DepartmentId=b.DepartmentId and a.Salary=b.max_s) aa join Department on aa.DepartmentId=Department.Id;
+'''select Department.Name as `Department`, aa.Name as `Employee`, aa.Salary from (select a.Name, a.Salary, a.DepartmentId from Employee a join (select DepartmentId, max(Salary) as max_s from Employee group by DepartmentId) b on a.DepartmentId=b.DepartmentId and a.Salary=b.max_s) aa join Department on aa.DepartmentId=Department.Id;'''
