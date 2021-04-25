0117596b54cd9d7bbcc67846dc1934b14e100a3e
luoyanhan <707025023@qq.com>
2021-04-25 10:47:54 +0800

    feat(Leetcode medium):254 官方题解的递归写得好

--- Leetcode/medium/524.py
+++ Leetcode/medium/524.py
@@ -1,12 +1,41 @@
 class Solution:
     def findLongestWord(self, s, dictionary):
-        length = len(s)
-        def can_build(s, child):
+        def is_subseq(word):
             idx = 0
-            for each in child:
-                while s[idx] != each and idx < length:
+            l = len(word)
+            for each in s:
+                if each == word[idx]:
                     idx += 1
-                if idx > length - 1:
-                    return False
-                idx += 1
-            return True
+                if idx >= l:
+                    break
+            return idx == len(word)
+        dictionary.sort(key=lambda x: (-len(x), x))
+        for each in dictionary:
+            if is_subseq(each):
+                return each
+        return ''
+
+
+class Solution:
+    def findLongestWord(self, s, dictionary):
+        def is_subseq(word):
+            idx = 0
+            l = len(word)
+            for each in s:
+                if each == word[idx]:
+                    idx += 1
+                if idx >= l:
+                    break
+            return idx == len(word)
+        res = ''
+        for each in dictionary:
+            if is_subseq(each) and len(each) >= len(res):
+                if len(each) > len(res):
+                    res = each
+                elif each < res:
+                    res = each
+        return res
+
+
+
+print(Solution().findLongestWord("abpcplea", ["a","b","c"]))
