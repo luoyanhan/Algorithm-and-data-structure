f22a834db3905b7a67290c344a5e9d42ac87ad3f
luoyanhan <707025023@qq.com>
2021-05-12 19:09:19 +0800

    feat(Leetcode medium):offer 59 unfinish

---
+++ Leetcode/medium/offer 59.py
@@ -0,0 +1,41 @@
+from collections import deque
+class MaxQueue:
+
+    def __init__(self):
+        self.indices = deque()
+        self.enteties = deque()
+
+
+    def max_value(self) -> int:
+        if not self.indices:
+            return -1
+        return self.indices[0]
+
+
+    def push_back(self, value: int) -> None:
+        self.enteties.append(value)
+        if not self.indices:
+            self.indices.append(value)
+        else:
+            if self.indices[-1] < value:
+                while self.indices and self.indices[-1] < value:
+                    self.indices.pop()
+                self.indices.append(value)
+
+
+
+    def pop_front(self) -> int:
+        if not self.enteties:
+            return -1
+        value = self.enteties.popleft()
+        if value == self.indices[0]:
+            self.indices.popleft()
+        return value
+
+
+
+# Your MaxQueue object will be instantiated and called as such:
+# obj = MaxQueue()
+# param_1 = obj.max_value()
+# obj.push_back(value)
+# param_3 = obj.pop_front()
