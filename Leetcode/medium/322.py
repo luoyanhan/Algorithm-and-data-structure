c411638a256c74da12b89cce4f7f6d4b3ad65eed
luoyanhan <707025023@qq.com>
2021-05-18 12:36:40 +0800

    feat(Leetcode medium):322

--- Leetcode/medium/322.py
+++ Leetcode/medium/322.py
@@ -45,10 +45,22 @@ class Solution:
 
 class Solution:
     def coinChange(self, coins, amount):
-        dp = [-1 for _ in range(amount+1)]
+        dp = [-1 for _ in range(amount + 1)]
         dp[0] = 0
-        for num in range(1, amount+1):
+        for num in range(1, amount + 1):
             for coin in coins:
-                if 0 <= num-coin < amount+1 and dp[num-coin] != -1:
-                    dp[num] = min(dp[num], dp[num-coin]+1) if dp[num] != -1 else dp[num-coin]+1
+                if 0 <= num - coin < amount + 1 and dp[num - coin] != -1:
+                    dp[num] = min(dp[num], dp[num - coin] + 1) if dp[
+                                                                      num] != -1 else \
+                    dp[num - coin] + 1
         return dp[amount]
+
+
+class Solution:
+    def coinChange(self, coins, amount):
+        dp = [10 ** 4 + 1 for _ in range(amount + 1)]
+        dp[0] = 0
+        for coin in coins:
+            for num in range(coin, amount + 1):
+                dp[num] = min(dp[num], dp[num - coin] + 1)
+        return dp[amount] if dp[amount] < 10 ** 4 + 1 else -1
