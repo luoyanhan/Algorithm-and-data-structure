8d4a59a139106c7b1b4d4cdf0f42fb51261fe979
luoyanhan <707025023@qq.com>
2021-05-22 23:28:57 +0800

    feat(Leetcode medium): 307 uncompleted

--- Leetcode/medium/307.py
+++ Leetcode/medium/307.py
@@ -14,8 +14,35 @@ class NumArray:
         return total
 
 
+import math
+class NumArray:
 
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        length_nums = len(nums)
+        l = math.sqrt(length_nums)
+        self.length = int(math.ceil(length_nums/l))
+        self.b = [0 for _ in range(self.length)]
+        for i in range(length_nums):
+            self.b[int(i/self.length)] += nums[i]
+
+    def update(self, index: int, val: int) -> None:
+        self.b[int(index/self.length)] += val-self.nums[index]
+        self.nums[index] = val
+
+
+    def sumRange(self, left: int, right: int) -> int:
+        total = 0
+        start_block = int(left/self.length)
+        end_block = int(right/self.length)
+        if start_block == end_block:
+            for i in range(left, right+1):
+                total += self.nums[i]
+        else:
+            for i in range(left, (start_block+1)*self.length):
+                total += self.nums[i]
+            for i in range(start_block+1, end_block):
+                total += self.b[i]
+            for i in range(end_block*self.length, right+1):
+                total += self.nums[i]
+        return total
