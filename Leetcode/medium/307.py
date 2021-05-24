97629eca30333790cc98de9aa5fd6e234ad293dd
luoyanhan <707025023@qq.com>
2021-05-24 17:44:24 +0800

    feat(Leetcode medium):307 Segment tree interesting

--- Leetcode/medium/307.py
+++ Leetcode/medium/307.py
@@ -46,3 +46,46 @@ class NumArray:
             for i in range(end_block*self.length, right+1):
                 total += self.nums[i]
         return total
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.length = len(nums)
+        self.tree = [0 for _ in range(2*self.length)]
+        for i in range(self.length, 2*self.length):
+            self.tree[i] = nums[i-self.length]
+        for i in range(self.length-1, 0, -1):
+            self.tree[i] = self.tree[2*i] + self.tree[2*i+1]
+
+
+
+    def update(self, index: int, val: int) -> None:
+        index += self.length
+        self.tree[index] = val
+        while index > 0:
+            if index % 2 == 0:
+                left = index
+                right = index + 1
+            else:
+                left = index - 1
+                right = index
+            self.tree[left//2] = self.tree[left] + self.tree[right]
+            index = index // 2
+
+
+
+    def sumRange(self, left: int, right: int) -> int:
+        left += self.length
+        right += self.length
+        total = 0
+        while left <= right:
+            if left % 2 == 1:
+                total += self.tree[left]
+                left += 1
+            if right % 2 == 0:
+                total += self.tree[right]
+                right -= 1
+            left = left // 2
+            right = right // 2
+        return total
