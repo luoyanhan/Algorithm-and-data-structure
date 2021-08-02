2f40833e166bec65a235c054f6958241616be647
luoyanhan <707025023@qq.com>
2021-08-02 20:32:38 +0800

    feat(leetcode medium): 300

--- Leetcode/medium/300.py
+++ Leetcode/medium/300.py
@@ -11,3 +11,14 @@ class Solution:
         return max(dp)
 
 
+class Solution:
+    def lengthOfLIS(self, nums):
+        length = len(nums)
+        dp = [1 for _ in range(length)]
+        for i in range(1, length):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
+
+
