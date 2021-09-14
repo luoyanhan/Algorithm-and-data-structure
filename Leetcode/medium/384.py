551d32152d0ffd5125619c5a469c6e346c3d7f37
罗延瀚 <luoyanhan@alphaleader.com>
2021-09-14 12:09:52 +0800

    feat(medium): 384 review

--- Leetcode/medium/384.py
+++ Leetcode/medium/384.py
@@ -17,3 +17,30 @@ class Solution:
             swap_idx = random.randint(i, self.length-1)
             self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
         return self.array
+
+
+import random
+class Solution:
+
+    def __init__(self, nums):
+        self.nums = nums[:]
+        self.cur_nums = nums
+
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        """
+        self.cur_nums = self.nums[:]
+        return self.cur_nums
+
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        """
+        length = len(self.cur_nums)
+        for idx in range(length):
+            rand_idx = random.randint(idx, length-1)
+            self.cur_nums[idx], self.cur_nums[rand_idx] = self.cur_nums[rand_idx], self.cur_nums[idx]
+        return self.cur_nums
