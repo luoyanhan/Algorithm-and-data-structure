2ef4bae58f1f5104dc70afcfe35b1a1bea0f9c1a
罗延瀚 <luoyanhan@alphaleader.com>
2021-09-14 15:17:39 +0800

    feat(medium): 99

---
+++ Leetcode/medium/99.py
@@ -0,0 +1,32 @@
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root):
+        mid_list = list()
+        stack = list()
+        node = root
+        while node or stack:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                mid_list.append(node)
+                node = node.right
+        loc1 = None
+        loc2 = None
+        for idx in range(1, len(mid_list)):
+            pre_node, cur_node = mid_list[idx-1], mid_list[idx]
+            if pre_node.val > cur_node.val:
+                if loc1 is None:
+                    loc1 = idx-1
+                else:
+                    loc2 = idx
+        if loc2 is None:
+            node1, node2 = mid_list[loc1], mid_list[loc1+1]
+        else:
+            node1, node2 = mid_list[loc1], mid_list[loc2]
+        node1.val, node2.val = node2.val, node1.val
